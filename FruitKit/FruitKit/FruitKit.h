ce94ab0c5a3ad1a57cf4920c61d87ca85195e7e4
Rufus <Rufusmall@hotmail.com>
2020-02-12 18:55:27 +0000

    Add fruitKit project. This includes a failing unit test to test CI is working as expected

---
+++ FruitKit/FruitKit/FruitKit.h
@@ -0,0 +1,19 @@
+//
+//  FruitKit.h
+//  FruitKit
+//
+//  Created by Rufus on 12/02/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FruitKit.
+FOUNDATION_EXPORT double FruitKitVersionNumber;
+
+//! Project version string for FruitKit.
+FOUNDATION_EXPORT const unsigned char FruitKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruitKit/PublicHeader.h>
+
+
---
+++ FruitKit/FruitKitTests/FruitKitTests.swift
@@ -0,0 +1,35 @@
+//
+//  FruitKitTests.swift
+//  FruitKitTests
+//
+//  Created by Rufus on 12/02/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+import XCTest
+@testable import FruitKit
+
+class FruitKitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        XCTAssertTrue(false)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
