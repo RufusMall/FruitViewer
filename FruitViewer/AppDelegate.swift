683a5a9aaf2f8264412df3152ad71cd883ff6c05
Rufus <Rufusmall@hotmail.com>
2020-02-12 18:19:13 +0000

    Merge setupCI branch. This includes initial project setup, and building on CI.
    Squashed commit of the following:
    
    commit 4fbbec3b42911b91c4b73f57bdf2a495b63c55eb
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Wed Feb 12 18:15:22 2020 +0000
    
        remove unused SceneDelegate
    
    commit c2a433024df42ef8827d742882e13082cb65129f
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Wed Feb 12 18:12:19 2020 +0000
    
        Add readme to the project, Show CI build status in readme, and include a link to the trello board
    
    commit 1c0aebbb3b94161bc554ea19670e31ef533641fa
    Author: Rufus <Rufusmall@hotmail.com>
    Date:   Wed Feb 12 17:59:58 2020 +0000
    
        setup initial project

---
+++ FruitViewer/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FruitViewer
+//
+//  Created by Rufus on 12/02/2020.
+//  Copyright © 2020 Rufus. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
